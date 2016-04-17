59457010962a329ad60a85a19c37da081aeb2bc3
Robert Rossmann <rr.rossmann@me.com>
2016-04-17 12:36:56 +0200

    Plain .eslintrc is deprecated, use .js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
-{
-  "env": {
-    "node": true
+'use strict'
+
+module.exports = {
+  env: {
+    node: true
   },
   // This config is linting itself according to the recommended rules since we are
   // unable to extends from a module inside of the .eslintrc file
   // Also avoids committing node_modules with a link to the modules
   // Ideal test would allow it to lint itself using its own rules
-  "extends": "eslint:recommended"
+  extends: 'eslint:recommended'
 }
