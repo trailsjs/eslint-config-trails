392c1b28c311eb3d1813bf992ada423393fb30b4
Travis J. Webb <me@traviswebb.com>
2017-01-03 01:41:09 -0500

    [rules] add TrailpackError global

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = {
     GraphCompletenessError: true,
     NamespaceConflictError: true,
     ValidationError: true,
+    TrailpackError: true,
 
     // trails types
     Service: true,
