222452244456ac6925a2916d38d0ef0cc282e937
Travis J. Webb <me@traviswebb.com>
2017-03-21 13:13:59 -0400

    [rules] remove newIsCap rule

--- rules/recommended.js
+++ rules/recommended.js
@@ -1,11 +1,8 @@
 module.exports = {
-  'extends': 'eslint:recommended',
+  extends: 'eslint:recommended',
   // overrides
-  'rules': {
+  rules: {
     // require or disallow use of semicolons instead of ASI
-    'semi': 0,
-    'new-cap': [0, {
-      'capIsNew': true
-    }]
+    semi: 0
   }
 }
